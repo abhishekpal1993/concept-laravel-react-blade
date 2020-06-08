160b8b345ae5f1f8434d612eb28573bb611dc722
Abhishek Pal <abhishekp@packt.com>
2020-06-08 18:28:39 +0530

    improving the react folder structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
+mix.react('resources/src/app.js', 'public/js')
     .version()
     .disableNotifications();
 
